d5c8d9aa9682d5011440c70220d3ba74293023dd
Arshabh Agarwal <arshabh.agarwal.it@gmail.com>
2016-02-25 02:43:58 +0530

    Renames directive.js to proper name and adds base scss file

---
+++ ng-material-grid.js
@@ -0,0 +1,12 @@
+(function () {
+  angular.module("ng-material-grid", []).
+      directive("ngMaterialGrid", function () {
+    return {
+      restrict: 'EA',
+      scope: {},
+      link: function (scope, element, attrs) {
+
+      }
+    }
+  })
+})();
